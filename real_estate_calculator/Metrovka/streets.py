7a707b4690f0a46191651686956e42ee4fae0e3d
AnastasiiaN <nav96@ukr.net>
2016-07-13 21:31:07 +0300

    Ready

--- real_estate_calculator/Metrovka/streets.py
+++
@@ -1,7 +0,0 @@
-from transliterate import translit
-
-with open('darn.txt') as f:
-    content = f.readlines()
-print content
-darn = [translit(x.encode(),"ru", reversed=True) for x in content]
-print darn
